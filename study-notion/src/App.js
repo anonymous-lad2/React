0b1900edf4e17b5e6c2ccc047419cde000603f9b
anonymous-lad2 <anonymouslad540@gmail.com>
2024-09-10 22:09:21 +0530

    Set up React-tailwind

---
+++ study-notion/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className= 'bg-black text-white text-[2rem] font-extrabold'>All Setup!!</h1>
+    </div>
+  );
+}
+
+export default App;
