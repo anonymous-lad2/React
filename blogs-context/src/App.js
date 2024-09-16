5e1a34f2553282ba23f853c6417858e5215426b2
anonymous-lad2 <anonymouslad540@gmail.com>
2024-09-16 17:31:46 +0530

    Blogs Context

---
+++ blogs-context/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-red-700 text-blue-600 p-6 font-bold text-[5rem]'>Lets start</h1>
+    </div>
+  );
+}
+
+export default App;
